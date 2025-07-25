0317f43a257bd4ef593ecc38ee9c6fe763271850
pulindu-seniya-silva <silva.m.k.p.s@gmail.com>
2025-07-25 14:09:23 +0530

    fix: Correct import name for ChatPromptTemplate and enhance prompt initialization in main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from anthropic import BaseModel
 from dotenv import load_dotenv
 import os
 import google.generativeai as genai
-from langchain_core.prompts import ChatPrompttemplate
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
-
+from langchain.agents import create_tool_calling_agent
 
 # Load API key from .env
 load_dotenv()
@@ -23,6 +23,23 @@ genai.configure(api_key=api_key)
 # Initialize the model
 model = genai.GenerativeModel("gemini-1.5-flash")
 parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
 
 # Make a call
 response = model.generate_content("What is the meaning of life?")
