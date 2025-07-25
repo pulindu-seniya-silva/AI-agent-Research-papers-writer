1925c9494f93eda21178b89d3b7333d40fd6d2dd
pulindu-seniya-silva <silva.m.k.p.s@gmail.com>
2025-07-25 12:29:37 +0530

    feat: Add ResearchResponse model and integrate PydanticOutputParser in main.py

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+from anthropic import BaseModel
 from dotenv import load_dotenv
 import os
 import google.generativeai as genai
+from langchain_core.prompts import ChatPrompttemplate
+from langchain_core.output_parsers import PydanticOutputParser
+
 
 # Load API key from .env
 load_dotenv()
+
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
 api_key = os.getenv("GEMINI_API_KEY")
 
 # Configure Gemini
@@ -11,6 +22,7 @@ genai.configure(api_key=api_key)
 
 # Initialize the model
 model = genai.GenerativeModel("gemini-1.5-flash")
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
 
 # Make a call
 response = model.generate_content("What is the meaning of life?")
